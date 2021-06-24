b7334486ea07439685e970600dc1c514bf02749a
Kaitlin Swanson <kateswanson94@gmail.com>
2021-06-23 17:56:13 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//form needs to collect: email. country, zip code, pw, confirm pw
+//add event listeners for categories assign constants
+const email = document.getElementById('email'); 
+const country = document.getElementById('country'); 
+const zipCode = document.getElementById('zipCode'); 
+const password = document.getElementById('password'); 
+const confirmPassword = document.getElementById('confirmPw'); 
+
+const submitButton = document.getElementById('submit');
+
+
+
+// check validation 
+
+//confirm pw needs to match pw field.  
+
+
+//submit button ex. if no errors change inner HTML to "submitted"
